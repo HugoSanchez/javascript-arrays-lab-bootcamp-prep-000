5bea3d79f583b6be91f0ee0a3d21768f6e9fa953
Hugo Sanchez <hugosanchez251@gmail.com>
2018-05-05 11:21:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = "I don't do much."
 kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name){
-  kittens = kitten.push(name);
+  kitten.push(name);
   return kittens
 }
